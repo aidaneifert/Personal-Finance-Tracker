3c9d0d1c91a248b2d75919c5d3e3fab81ebed238
FZNBowl <87474262+FZNBowl@users.noreply.github.com>
2026-01-28 23:49:59 -0500

    Fixed edge cases TODO: try to make it remember entered values and only ask for ones not provided for add_income & add_expense classes

--- src/Personal Finance Tracker/main.py
+++ src/Personal Finance Tracker/main.py
@@ -4,9 +4,17 @@ class FinanceTracker():
 
     def __init__(self):
         self.entries= []
-        with open('Data.json', 'r') as f: 
-            self.entries = json.load(f)
 
+        try:
+            with open('Data.json', 'r') as f: 
+                self.entries = json.load(f)
+
+        except FileNotFoundError:
+            print("WARNING: json storage file does not exist,"
+                  " a new blank json was made.")
+            with open("Data.json", "w") as f:
+                json.dump([], f, indent= 4)
+                
     def user_input(self):
         i = input(">")
         return i.strip()
@@ -15,34 +23,7 @@ class FinanceTracker():
         with open('Data.json', 'w') as f:
             json.dump(self.entries, f, indent= 4)
 
-    def add_income(self):
-        id = [d["id"] for d in self.entries]
-   
-        if not id:
-            new_id = 1
-        else:
-            i= len(self.entries)
-            new_id= i + 1
-
-        new_type= "income"
-        new_amount= input("Amount >")
-        new_source=input("Source >")
-        new_date=input("Date >")
-        new_description=input("Description >")
-
-        new_input ={
-        "id":new_id,
-        "type":new_type,
-        "amount":new_amount, 
-        "source":new_source,
-        "date":new_date, 
-        "description":new_description
-        }
-        
-        self.entries.append(new_input)
-        self.save_to_file()
-
-    def add_expense(self):
+    def add_income(self):       #work to do
         id = [d["id"] for d in self.entries]
 
         if not id:
@@ -51,49 +32,107 @@ class FinanceTracker():
             i= len(self.entries)
             new_id= i + 1
 
-        new_type= "expense"
-        new_amount= input("Amount >")
-        new_category=input("Category >")
-        new_date=input("Date >")
-        new_description=input("Description >")
+        while True:
+            new_type= "income"
+            new_amount= input("Amount >")
+            new_source=input("Source >")
+            new_date=input("Date >")
+            new_description=input("Description >")
 
-        new_input ={
-        "id":new_id,
-        "type":new_type,
-        "amount":new_amount, 
-        "category":new_category,
-        "date":new_date, 
-        "description":new_description
-        }
-        
-        self.entries.append(new_input)
-        self.save_to_file()
+            new_input ={
+            "id":new_id,
+            "type":new_type,
+            "amount":new_amount, 
+            "source":new_source,
+            "date":new_date, 
+            "description":new_description
+            }
+
+            empty_counter= [key for key, value in new_input.items() if value == ""]
+
+            if empty_counter:
+                for key in empty_counter:
+                    if key == "amount":
+                        print(f"you did not add an {key}")
+                    else:
+                        print(f"you did not add a {key}")
+                continue
+            
+            elif not empty_counter:
+                self.entries.append(new_input)
+                self.save_to_file()
+                break
+            
+        # try to make it remember entered values and only ask for ones not provided
+
+    def add_expense(self):      #work to do
+        id = [d["id"] for d in self.entries]
+
+        if not id:
+            new_id = 1
+        else:
+            i= len(self.entries)
+            new_id= i + 1
+            
+        while True:
+            new_type= "expense"
+            new_amount= input("Amount >")
+            new_category=input("Category >")
+            new_date=input("Date >")
+            new_description=input("Description >")
+
+            new_input ={
+            "id":new_id,
+            "type":new_type,
+            "amount":new_amount, 
+            "category":new_category,
+            "date":new_date, 
+            "description":new_description
+            }
+
+            empty_counter= [key for key, value in new_input.items() if value == ""]
+
+            if empty_counter:
+                for key in empty_counter:
+                    if key == "amount":
+                        print(f"you did not add an {key}")
+                    else:
+                        print(f"you did not add a {key}")
+                continue
+
+            elif not empty_counter:
+                self.entries.append(new_input)
+                self.save_to_file()
+                break
+
+        # try to make it remember entered values and only ask for ones not provided
 
     def show_entries(self):
+        if not self.entries:
+            print("You have no entries")
+        else:
+            for dicts in self.entries:
+                for key,transacts in dicts.items():
+                    print(f"{key}:{transacts}")
+            while True:
+                print("\ntype e to edit, type r to remove, press enter to exit")
+                i = self.user_input().lower()
 
-        for dicts in self.entries:
-            for key,transacts in dicts.items():
-                print(f"{key}:{transacts}")
-
-        print("\ntype e to edit, type r to remove, press enter to exit")
-        i = self.user_input().lower()
-
-        match i:
-            case "e":
-                self.edit_entry()
-            case "r":
-                self.remove_entry()
-            case "":
-                return 
-            case _: 
-                print("Invalid input")
+                match i:
+                    case "e":
+                        self.edit_entry()
+                    case "r":
+                        self.remove_entry()
+                    case "":
+                        return 
+                    case _: 
+                        print("Invalid input")
 
     def generate_report(self):
         expenses= [d for d in self.entries if d.get("type") == "expense"]
         income= [d for d in self.entries if d.get("type") == "income"]
         total_income= 0
         total_expenses= 0
-        remaining_money= total_income - total_expenses
 
         for i in income:
             for key, value in i.items():
@@ -105,81 +144,133 @@ class FinanceTracker():
                 if key == "amount":
                     total_expenses= total_expenses + int(value)
 
+        remaining_money= total_income - total_expenses
         print(f"You have made {total_income} \nYou have spent {total_expenses} \nYou have {remaining_money} remaining")
         input("Press Enter to continue")
-
+     
     def edit_entry(self):
         print("Enter a transaction id to edit") 
-        user_input = self.user_input()
+        user_input= self.user_input()
 
         try:
             to_remove = int(user_input)
         except ValueError:
             print("Invalid Input")
+
+        if to_remove > len(self.entries):
+            print("the id you entered does not exist")
+            return
+        
+        elif to_remove <= 0:
+            print("the id you entered does not exist")
+            return
         
         for i, d in enumerate(self.entries):
             if d.get("id") == to_remove:
-                entry = self.entries.pop(i)
+                entry= self.entries.pop(i)
 
         dict_id= entry.get("id")
-        dict_type=  entry.get("type")
+        dict_type= entry.get("type")
 
         match dict_type:
             case "expense":
-                amount= entry.get("ammount")
-                category= entry.get("category")
-                date= entry.get("date")
-                description= entry.get("description")
+                while True:
+                    amount= entry.get("amount")
+                    category= entry.get("category")
+                    date= entry.get("date")
+                    description= entry.get("description")
 
-                new_amount= input(f"Previous value: {amount} >")
-                new_category= input(f"Previous value: {category} >")
-                new_date= input(f"Previous value: {date} >")
-                new_description= input(f"Previous value: {description} >")
+                    new_amount= input(f"Previous value: {amount} >")
+                    new_category= input(f"Previous value: {category} >")
+                    new_date= input(f"Previous value: {date} >")
+                    new_description= input(f"Previous value: {description} >")
 
-                new_input ={
-                "id":dict_id,
-                "type":dict_type,
-                "amount":new_amount, 
-                "category":new_category,
-                "date":new_date, 
-                "description":new_description
-                }
+                    new_input= {
+                    "id":dict_id,
+                    "type":dict_type,
+                    "amount":new_amount, 
+                    "category":new_category,
+                    "date":new_date, 
+                    "description":new_description
+                    }
+                    empty_counter= [key for key, value in new_input.items() if value == ""]
+
+                    if empty_counter:
+                        for key in empty_counter:
+                            if key == "amount":
+                                print(f"you did not add an {key}")
+                            else:
+                                print(f"you did not add a {key}")
+                        continue
+
+                    elif not empty_counter:
+                        self.entries.append(new_input)
+                        self.save_to_file()
+                        break    
 
             case "income":
-                amount= entry.get("ammount")
-                source= entry.get("source")
-                date= entry.get("date")
-                description= entry.get("description")
+                while True:
+                    amount= entry.get("amount")
+                    source= entry.get("source")
+                    date= entry.get("date")
+                    description= entry.get("description")
 
-                new_amount= input(f"Previous value: {amount} >")
-                new_source= input(f"Previous value: {source} >")
-                new_date= input(f"Previous value: {date} >")
-                new_description= input(f"Previous value: {description} >")
+                    new_amount= input(f"Previous value: {amount} >")
+                    new_source= input(f"Previous value: {source} >")
+                    new_date= input(f"Previous value: {date} >")
+                    new_description= input(f"Previous value: {description} >")
 
-                new_input={
-                "id":dict_id,
-                "type":dict_type,
-                "amount":new_amount, 
-                "source":new_source,
-                "date":new_date,
-                "description":new_description
-                }
+                    new_input={
+                    "id":dict_id,
+                    "type":dict_type,
+                    "amount":new_amount, 
+                    "source":new_source,
+                    "date":new_date,
+                    "description":new_description
+                    }
 
-        self.entries.insert(i, new_input)
-        self.save_to_file()
+                    empty_counter= [key for key, value in new_input.items() if value == ""]
 
+                    if empty_counter:
+                        for key in empty_counter:
+                            if key == "amount":
+                                print(f"you did not add an {key}")
+                            else:
+                                print(f"you did not add a {key}")
+                        continue
+
+                    elif not empty_counter:
+                        self.entries.append(new_input)
+                        self.save_to_file()
+                        break                
+ 
     def remove_entry(self):
+        new_id= 1
         print("Enter a transaction id to remove") 
 
         try:
-            input = int(self.user_input())
+            to_remove = int(self.user_input())
+
         except ValueError:
             print("Invalid input")
 
+        if to_remove > len(self.entries):
+            print("the id you entered does not exist")
+            return
+
+        elif to_remove <= 0:
+            print("the id you entered does not exist")
+            return
+        
         for i, d in enumerate(self.entries):
-            if d.get("id") == input:
+            if d.get("id") == to_remove:
                 self.entries.pop(i)
-                self.save_to_file()
+
+        for d in self.entries:
+            d["id"]= new_id
+            new_id= new_id + 1
+            
+        self.save_to_file()
 
     def main_menu(self):
         while True:
